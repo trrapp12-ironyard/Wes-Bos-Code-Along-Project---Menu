c36eee35f6c9457956a6d32110471566bb8e3f62
trrapp@gmail.com <trrapp@gmail.com>
2016-11-16 08:21:29 -0700

    add router

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import {BrowserRouter, Match, Miss} from 'react-router';
 import './css/style.css';
 import App from './components/App';
+import NotFound from './components/NotFound';
 
 import StorePicker from './components/StorePicker';
+
+const Root = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Match exactly pattern='/' component={StorePicker} />
+        <Match pattern='/store/:storeId' component={App} />
+        <Miss component={notFound} />
+      </BrowserRouter>
+    </div>
+  )
+}
 
 render(<App/>, document.querySelector('#main'));
