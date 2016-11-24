dd9117cd7476d5c98e31bd9f071a52afb4fdc5f9
trrapp@gmail.com <trrapp@gmail.com>
2016-11-24 00:10:34 -0700

    added state

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
   goToStore(event) {
     event.preventDefault();
+    console.log('You Changed the URL');
+    // first grab the text from the box
     const storeId = this.storeInput.value;
-
-    this.context.router.transitionTo('/store/${storeId}');
+    console.log(`Going to ${storeId}`)
+    // second we're going to transition from / to /store/:storeId
+    this.context.router.transitionTo(`/store/${storeId}`);
   }
 
   render() {
@@ -14,7 +21,7 @@ class StorePicker extends React.Component {
     return (
       <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
         <button type="submit">Visit Store →</button>
       </form>
     )
