a10d1a8dd2ae306a4b1aa864d021556688b19b64
trrapp@gmail.com <trrapp@gmail.com>
2016-11-29 21:40:07 -0700

    add firebase and base.js

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,12 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+
+    apiKey: "AIzaSyACC5QEwUuQRDWbqhBo4RttSpnrC4bis94",
+    authDomain: "boutique-e3f7d.firebaseapp.com",
+    databaseURL: "https://boutique-e3f7d.firebaseio.com",
+    storageBucket: "boutique-e3f7d.appspot.com",
+    messagingSenderId: "227473600092"
+});
+
+export default base;
