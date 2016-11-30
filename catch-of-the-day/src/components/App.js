a7209ce30ca92235f77651783fc7f2651a5cd854
trrapp@gmail.com <trrapp@gmail.com>
2016-11-30 01:07:04 -0700

    add two way binding

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.updateFish = this.updateFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
     // getinitialState
@@ -55,6 +56,12 @@ componentWillUpdate(nextProps, nextState) {
     this.setState({ fishes });
   }
 
+  updateFish(key, updatedFish) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFish;
+    this.setState({ fishes });
+  }
+
   loadSamples() {
     this.setState({
       fishes. sampleFishes
@@ -85,7 +92,8 @@ componentWillUpdate(nextProps, nextState) {
         </div>
         <Order fishes={this.state.fishes} order={this.state.order}
         params={this.props.params}/>
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes}
+        updateFish={this.updateFish}/>
       </div>
     )
   }
